f85742fd5f3018806d4ae3c4b6ae6a75058e24a9
mbelesiu <matt.belesiu@gmail.com>
2020-10-07 16:32:56 -0700

    updated to actually be a proxy

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const axios = require('axios');
 
 const app = express();
 app.use(bodyParser.json());
@@ -12,27 +13,45 @@ app.use(express.static(path.join(__dirname, '/../public')));
 
 // Review Service Routes
 app.get('/api/products/reviews/:id/:count/:sort', (req, res) => {
-  res.redirect(`http://localhost:3005${req.url}`);
+  // res.redirect(`http://localhost:3005${req.url}`);
+  axios.get(`http://localhost:3005${req.url}`)
+      .then(({ data }) => res.send(data))
+      .catch((err) => (err));
 });
 app.get('/api/products/reviews/avg/:item', (req, res) => {
-   res.redirect(`http://localhost:3005${req.url}`);
+  //  res.redirect(`http://localhost:3005${req.url}`);
+   axios.get(`http://localhost:3005${req.url}`)
+      .then(({ data }) => res.send(data))
+      .catch((err) => (err));
 });
 // Display Service Routes
 app.get('/api/products/:id', async (req, res) => {
-  res.redirect(`http://localhost:3002${req.url}`);
+  // res.redirect(`http://localhost:3002${req.url}`);
+  axios.get(`http://localhost:3002${req.url}`)
+  .then(({ data }) => res.send(data))
+  .catch((err) => (err));
 });
 // Questyions Service Routes
 app.get('/api/products/questions/sort/:sort', (req, res) => {
-  res.redirect(`http://localhost:3001${req.url}`);
+  // res.redirect(`http://localhost:3001${req.url}`);
+  axios.get(`http://localhost:3001${req.url}`)
+  .then(({ data }) => res.send(data))
+  .catch((err) => (err));
 });
 
 app.get('/api/products/questions/:question_id', (req, res) => {
-  res.redirect(`http://localhost:3001${req.url}`);
+  // res.redirect(`http://localhost:3001${req.url}`);
+  axios.get(`http://localhost:3001${req.url}`)
+  .then(({ data }) => res.send(data))
+  .catch((err) => (err));
 });
 
 //Realted Items Routes
 app.get('/api/products/:id/relatedItems', (req, res) => {
-  res.redirect(`http://localhost:3004${req.url}`);
+  //res.redirect(`http://localhost:3004${req.url}`);
+  axios.get(`http://localhost:3004${req.url}`)
+  .then(({ data }) => res.send(data))
+  .catch((err) => (err));
 });
 
 
